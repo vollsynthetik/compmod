df3bc40428d8d49489dd69bff87f9afca9fe0335
Ralf Müller <restless.visitor@gmail.com>
2021-02-03 20:49:14 +0000

    added midi pitch enumeration

---
+++ src/compmod/Enums/Pitch.cs
@@ -0,0 +1,157 @@
+using System;
+
+namespace CompMod.Enums
+{
+  public enum Pitch
+  {
+    // octave -1
+    C1Neg = 0,
+    DFlat1Neg = 1,
+    D1Neg = 2,
+    EFlat1Neg = 3,
+    E1Neg = 4,
+    F1Neg = 5,
+    GFlat1Neg = 6,
+    G1Neg = 7,
+    AFlat1Neg = 8,
+    A1Neg = 9,
+    BFlat1Neg = 10,
+    B1Neg = 11,
+
+    // octave 0
+    C0 = 12,
+    DFlat0 = 13,
+    D0 = 14,
+    EFlat0 = 15,
+    E0 = 16,
+    F0 = 17,
+    GFlat0 = 18,
+    G0 = 19,
+    AFlat0 = 20,
+    A0 = 21,
+    BFlat0 = 22,
+    B0 = 23,
+
+    // octave 1
+    C1 = 24,
+    DFlat1 = 25,
+    D1 = 26,
+    EFlat1 = 27,
+    E1 = 28,
+    F1 = 29,
+    GFlat1 = 30,
+    G1 = 31,
+    AFlat1 = 32,
+    A1 = 33,
+    BFlat1 = 34,
+    B1 = 35,
+
+    // octave 2
+    C2 = 36,
+    DFlat2 = 37,
+    D2 = 38,
+    EFlat2 = 39,
+    E2 = 40,
+    F2 = 41,
+    GFlat2 = 42,
+    G2 = 43,
+    AFlat2 = 44
+    A2 = 45,
+    BFlat2 = 46,
+    B2 = 47,
+
+    // octave 3
+    C3 = 48,
+    DFlat3 = 49,
+    D3 = 50,
+    EFlat3 = 51,
+    E3 = 52,
+    F3 = 53,
+    GFlat3 = 54,
+    G3 = 55,
+    AFlat3 = 56,
+    A3 = 57,
+    BFlat3 = 58,
+    B3 = 59,
+
+    // octave 4
+    C4 = 60,
+    DFlat4 = 61,
+    D4 = 62,
+    EFlat4 = 63,
+    E4 = 64,
+    F4 = 65,
+    GFlat4 = 66,
+    G4 = 67,
+    AFlat4 = 68,
+    A4 = 69,
+    BFlat4 = 70,
+    B4 = 71,
+
+    // octave 5
+    C5 = 72,
+    DFlat5 = 73,
+    D5 = 74,
+    EFlat5 = 75,
+    E5 = 76,
+    F5 = 77,
+    GFlat5 = 78,
+    G5 = 79,
+    AFlat5 = 80,
+    A5 = 81,
+    BFlat5 = 82,
+    B5 = 83,
+
+    // octave 6
+    C6 = 84,
+    DFlat6 = 85,
+    D6 = 86,
+    EFlat6 = 87,
+    E6 = 88,
+    F6 = 89,
+    GFlat6 = 90,
+    G6 = 91,
+    AFlat6 = 92,
+    A6 = 93,
+    BFlat6 = 94,
+    B6 = 95,
+
+    // octave 7
+    C7 = 96,
+    DFlat7 = 97,
+    D7 = 98,
+    EFlat7 = 99,
+    E7 = 100,
+    F7 = 101,
+    GFlat7 = 102,
+    G7 = 103,
+    AFlat7 = 104,
+    A7 = 105,
+    BFlat7 = 106,
+    B7 = 107,
+
+    // octave 8
+    C8 = 108,
+    DFlat8 = 109,
+    D8 = 110,
+    EFlat8 = 111,
+    E8 = 112,
+    F8 = 113,
+    GFlat8 = 114,
+    G8 = 115,
+    AFlat8 = 116,
+    A8 = 117,
+    BFlat8 = 118,
+    B8 = 119,
+
+    // octave 9
+    C9 = 120,
+    DFlat9 = 121,
+    D9 = 122,
+    EFlat9 = 123,
+    E9 = 124,
+    F9 = 125,
+    GFlat9 = 126,
+    G9 = 127
+  }
+}

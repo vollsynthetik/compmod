700c674655338c51cec7da2efc31891f6d951f80
Ralf Müller <restless.visitor@gmail.com>
2021-02-04 20:35:09 +0000

    tool class for musical durations added

---
+++ src/compmod/Tools/Durations.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace CompMod.Tools
+{
+  public static class Durations
+  {
+    public static double WholeNote = 1;
+    public static double HalfNote = 0.5;
+    public static double QuarterNote = 0.25;
+    public static double EighthNote = 0.125;
+    public static double One16thNote = 0.0625;
+    public static double One32thNote = 0.03125;
+    public static double One64thNote = 0.015625;
+    public static double One128thNote = 0.0078125;
+    public static double One256thNote = 0.00390625;
+    public static double One512thNote = 0.001953125;
+  }
+}

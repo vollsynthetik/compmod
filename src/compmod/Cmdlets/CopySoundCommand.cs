fc84a46c6151b8a5cf7cdf0b11ab3eb6dee9474b
Ralf Müller <restless.visitor@gmail.com>
2021-02-07 12:26:00 +0000

    changed copy note to more abstract copy sound

--- src/compmod/Cmdlets/CopySoundCommand.cs
+++ src/compmod/Cmdlets/CopySoundCommand.cs
@@ -4,15 +4,15 @@ using CompMod.Objects;
 
 namespace CompMod.Cmdlets
 {
-  [Cmdlet(Verbs.Common.Copy, "Note")]
-  public class CopyNoteCommand : Cmdlet
+  [Cmdlet(Verbs.Common.Copy, "Sound")]
+  public class CopySoundCommand : Cmdlet
   {
     [Parameter(Mandatory=true, Position = 0)]
-    public Note Note { get; set; }
+    public Sound Sound { get; set; }
 
     protected override void ProcessRecord()
     {
-      WriteObject(new Note(Note.Pitch, Note.Velocity, Note.Duration));
+      WriteObject(sound.Copy());
     }
   }
 }
